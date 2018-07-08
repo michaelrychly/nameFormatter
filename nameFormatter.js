2a1942a50bd8f77973a697da666b6a1c78c59fc1
michaelrychly <MichaelRychly@gmail.com>
2018-07-07 21:31:46 -0400

    all test in green and functionality implemented

--- nameFormatter.js
+++ nameFormatter.js
@@ -1,8 +1,42 @@
 class NameFormatter {
 
   invertName(name) {
-    return name;
+    let nameArray = [];
+
+    if (name === undefined) {
+      throw 'Error';
+    } else if (name.substring(0, 1).match(/\s/)) {
+      nameArray = name.substring(1, name.length).split(' ');
+
+      if (nameArray.length === 4) {
+        return nameArray[0] + " " + nameArray[2] + ", " + nameArray[1];
+      } else if (nameArray.length === 2 && nameArray[1] !== '') {
+        return nameArray[1] + ", " + nameArray[0];
+      } else {
+        return name.trim();
+      }
+    } else if (name.match(/[A-Z]{1}[a-z]{1,2}\./)) {
+      nameArray = name.substring(0, name.length).split(' ');
+
+      if(nameArray.length === 2) {
+        return name;
+      } else if (nameArray.length === 3) {
+        return nameArray[0] + " " + nameArray[2] + ", " + nameArray[1];
+      } else {
+        return "";
+      }
+    } else {
+      nameArray = name.split(' ');
+
+      if (nameArray.length === 2) {
+        return nameArray[1] + ", " + nameArray[0];
+      } else {
+        return name.trim();
+      }
+    }
   }
 }
 
+//let nameFormatter = new NameFormatter();
+//nameFormatter.invertName(undefined);
 module.exports = NameFormatter;
--- test/nameFormatterTest.js
+++ test/nameFormatterTest.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
+const expect = chai.expect;
 const NameFormatter = require('../nameFormatter');
 
 describe('invert name', function(){
@@ -17,5 +18,68 @@ describe('invert name', function(){
     const expectedOutput = "name";
 
     assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should return a single name when passed a single name with extra spaces', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = " name ";
+    const expectedOutput = "name";
+
+    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should return a last-name, first-name when passed a first and last-name', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = "first-name last-name";
+    const expectedOutput = "last-name, first-name";
+
+    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should return a last-name, first-name when passed a first and last-name with extra spaces around the names', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = " first-name last-name";
+    const expectedOutput = "last-name, first-name";
+
+    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should return an empty string when passed a single honorific', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = "Dr.";
+    const expectedOutput = "";
+
+    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should return honorific first-name when passed honorific first-name', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = "Dr. first-name";
+    const expectedOutput = "Dr. first-name";
+
+    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should return a honorific last-name, first-name when passed honorific first-name last-name', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = "Dr. first-name last-name";
+    const expectedOutput = "Dr. last-name, first-name";
+
+    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should return a honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = " Dr. first-name last-name ";
+    const expectedOutput = "Dr. last-name, first-name";
+
+    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+  })
+
+  it('should throw an error when name is undefined', function() {
+    const nameFormatter = new NameFormatter();
+    const inputName = undefined;
+
+    expect(function() { nameFormatter.invertName(inputName); }).to.throw('Error');
   })
 });
